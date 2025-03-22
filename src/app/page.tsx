be9c8ff5d391ff3bb87261a1ebf6581c72b8402e
declspecl <gavind2559@gmail.com>
2025-03-22 13:49:59 -0400

    init: shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 
 export default function Home() {
   return (
-      <div className="flex flex-row gap-8">
-        <p>test</p>
-        <p>test</p>
-      </div>
+    <div className="bg-red-500">
+      <Card>
+        <CardHeader>
+          <CardTitle>This is a card</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Button variant="default">Some button</Button>
+
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant="outline">Open Dialog</Button>
+            </DialogTrigger>
+            <DialogContent>
+              <p>some dialog content</p>
+            </DialogContent>
+          </Dialog>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
