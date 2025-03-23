0b896014cbd8338835966d72ad87a0f8d53693d6
Lisa Nguyen <nguyen8@oakland.edu>
2025-03-23 01:05:44 -0400

    fix: add home page back

---
+++ src/app/page.tsx
@@ -0,0 +1,27 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+
+export default function Home() {
+    return (
+        <div className="bg-red-500">
+            <Card>
+                <CardHeader>
+                    <CardTitle>This is a card</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <Button variant="default">Some button</Button>
+
+                    <Dialog>
+                        <DialogTrigger asChild>
+                            <Button variant="outline">Open Dialog</Button>
+                        </DialogTrigger>
+                        <DialogContent>
+                            <p>some dialog content</p>
+                        </DialogContent>
+                    </Dialog>
+                </CardContent>
+            </Card>
+        </div>
+    );
+}
